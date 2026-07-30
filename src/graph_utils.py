234101a766dee1c709eb17500954a640a9f4b054
HydroCation <131238150+HydroCation@users.noreply.github.com>
2026-07-30 13:27:24 +0000

    Add interactive Erdős-Rényi simulation with custom node color mapping and pos support

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 def generate_random_graph(n = 50, p = 0.1): #n = nodes, p = probability of edge between any two nodes
     return nx.erdos_renyi_graph(n=n, p=p)
 
-def visualize_graph(graph, status = None): #status = dict mapping each node to S I or T
+def visualize_graph(graph, status = None, pos = None): #status = dict mapping each node to S I or T
     color_map = {
-        "S":"blue",
+        "S":"yellow",
         "I": "red",
         "R": "green"
     }
@@ -14,5 +14,5 @@ def visualize_graph(graph, status = None): #status = dict mapping each node to S
     for node in graph.nodes():
         state = status[node] if status and node in status else "S" #If status is given and node in status, use that else assume node is susceptible
         node_colors.append(color_map[state])
-    nx.draw(graph, node_color = node_colors, with_labels = True)
+    nx.draw(graph, pos = pos, node_color = node_colors, with_labels = True)
     plt.show()
